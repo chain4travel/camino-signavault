3875aac7473391a4980db10a328747742e0c5228
Anastasios Karafyllidis <anastasios.karafyllidis@prodyna.com>
2023-02-10 17:02:16 +0200

    TECH-506 Removed dbr package and refactored queries using go sql package instead

--- model/multisig_tx.go
+++ model/multisig_tx.go
@@ -1,17 +1,17 @@
 package model
 
 type MultisigTx struct {
-	Id            int64              `json:"id"	 		`
-	Alias         string             `json:"alias" 		`
-	Threshold     int                `json:"threshold" 	`
-	Signers       []MultisigTxSigner `json:"signers" 		`
-	TransactionId string             `json:"transactionId" "`
-	UnsignedTx    string             `json:"unsignedTx" 	`
+	Id            int64              `json:"id"`
+	Alias         string             `json:"alias"`
+	Threshold     int                `json:"threshold"`
+	Signers       []MultisigTxSigner `json:"signers"`
+	TransactionId string             `json:"transactionId"`
+	UnsignedTx    string             `json:"unsignedTx"`
 }
 
 type MultisigTxSigner struct {
-	Id           int64
-	MultisigTxId int64
+	Id           int64  `json:"_"`
+	MultisigTxId int64  `json:"_"`
 	Address      string `json:"address"`
 	Signature    string `json:"signature"`
 }
--- service/multisig_service.go
+++ service/multisig_service.go
@@ -1,9 +1,10 @@
 package service
 
 import (
+	"database/sql"
 	"github.com/chain4travel/camino-signavault/db"
 	"github.com/chain4travel/camino-signavault/model"
-	"github.com/gocraft/dbr/v2"
+	"log"
 )
 
 type MultisigService struct {
@@ -15,60 +16,144 @@ func NewMultisigService() *MultisigService {
 
 func (s *MultisigService) CreateMultisigTx(multisigTx *model.MultisigTx) (int64, error) {
 	var err error
-	session := db.GetInstance().NewSession(nil)
-	tx, err := session.Begin()
 
-	defer tx.RollbackUnlessCommitted()
-
-	exec, err := tx.InsertInto("multisig_tx").
-		Pair("alias", multisigTx.Alias).
-		Pair("threshold", multisigTx.Threshold).
-		Pair("transaction_id", multisigTx.TransactionId).
-		Pair("unsigned_tx", multisigTx.UnsignedTx).
-		Exec()
+	tx, err := db.GetInstance().Begin()
 	if err != nil {
 		return 0, err
 	}
+	defer func(tx *sql.Tx) {
+		err := tx.Rollback()
+		if err != nil {
+			log.Print(err)
+		}
+	}(tx)
 
-	id, _ := exec.LastInsertId()
+	stmt, err := tx.Prepare("INSERT INTO multisig_tx (alias, threshold, transaction_id, unsigned_tx) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return 0, err
+	}
+	res, err := stmt.Exec(multisigTx.Alias, multisigTx.Threshold, multisigTx.TransactionId, multisigTx.UnsignedTx)
+	if err != nil {
+		return 0, err
+	}
+	txId, _ := res.LastInsertId()
 
 	for _, signer := range multisigTx.Signers {
-		exec, err = tx.InsertInto("multisig_tx_signers").
-			Pair("multisig_tx_id", id).
-			Pair("address", signer.Address).
-			Pair("signature", signer.Signature).
-			Exec()
+		stmt, err := tx.Prepare("INSERT INTO multisig_tx_signers (multisig_tx_id, address, signature) VALUES (?, ?, ?)")
+		if err != nil {
+			return 0, err
+		}
+		_, err = stmt.Exec(txId, signer.Address, signer.Signature)
 		if err != nil {
 			return 0, err
 		}
 	}
-
 	err = tx.Commit()
-
 	if err != nil {
 		return 0, err
 	}
-	return id, nil
+
+	return txId, nil
+}
+func (s *MultisigService) GetAllMultisigTx() (*[]model.MultisigTx, error) {
+	return s.doGetMultisigTx("")
 }
 
-func (s *MultisigService) GetMultisigTx(alias string) (*[]model.MultisigTx, error) {
-	session := db.GetInstance().NewSession(nil)
-	var err error
-
-	_, err = session.Begin()
-
-	multisigTx := &[]model.MultisigTx{}
-
-	_, err = session.
-		Select("multisig_tx.id", "alias", "threshold", "transaction_id", "unsigned_tx", "signers.multisig_tx_id", "signers.id", "signers.address", "signers.signature").
-		From("multisig_tx").
-		LeftJoin(dbr.I("multisig_tx_signers").As("signers"), "multisig_tx.id = signers.multisig_tx_id").
-		Where("alias=?", alias).
-		Load(multisigTx)
+func (s *MultisigService) GetMultisigTx(alias string) (*model.MultisigTx, error) {
+	tx, err := s.doGetMultisigTx(alias)
 
 	if err != nil {
 		return nil, err
 	}
+	if len(*tx) <= 0 {
+		return nil, nil
+	}
 
-	return multisigTx, nil
+	return &(*tx)[0], nil
+}
+
+func (s *MultisigService) doGetMultisigTx(alias string) (*[]model.MultisigTx, error) {
+	var err error
+
+	query := "SELECT tx.id, " +
+		"tx.alias, " +
+		"tx.threshold, " +
+		"tx.transaction_id, " +
+		"tx.unsigned_tx, " +
+		"signers.multisig_tx_id, " +
+		"signers.id, " +
+		"signers.address, " +
+		"signers.signature " +
+		"FROM multisig_tx AS tx " +
+		"LEFT JOIN multisig_tx_signers AS signers ON tx.id = signers.multisig_tx_id " +
+		"WHERE tx.alias=? OR ?=''" +
+		"ORDER BY tx.created_at ASC"
+
+	rows, err := db.GetInstance().Query(query, alias, alias)
+	if err != nil {
+		return nil, err
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			log.Print(err)
+		}
+	}(rows)
+
+	var result []model.MultisigTx
+	var multiSigTx = make(map[int]model.MultisigTx)
+
+	for rows.Next() {
+		var (
+			txId               int
+			txAlias            string
+			txThreshold        int
+			txTransactionId    string
+			txUnsignedTx       string
+			signerMultisigTxId int
+			signerId           int
+			signerAddress      string
+			signerSignature    string
+		)
+
+		err := rows.Scan(&txId, &txAlias, &txThreshold, &txTransactionId, &txUnsignedTx, &signerMultisigTxId, &signerId, &signerAddress, &signerSignature)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var tx model.MultisigTx
+		if _, ok := multiSigTx[txId]; ok {
+			tx = multiSigTx[txId]
+		} else {
+			tx = model.MultisigTx{
+				Id:            int64(txId),
+				Alias:         txAlias,
+				Threshold:     txThreshold,
+				TransactionId: txTransactionId,
+				UnsignedTx:    txUnsignedTx,
+			}
+
+		}
+		signers := tx.Signers
+		if (signers) == nil {
+			signers = []model.MultisigTxSigner{}
+		}
+		signers = append(signers, model.MultisigTxSigner{
+			Id:           int64(signerId),
+			MultisigTxId: int64(signerMultisigTxId),
+			Address:      signerAddress,
+			Signature:    signerSignature,
+		})
+		tx.Signers = signers
+		multiSigTx[txId] = tx
+
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// convert map to slice
+	for _, tx := range multiSigTx {
+		result = append(result, tx)
+	}
+	return &result, nil
 }
