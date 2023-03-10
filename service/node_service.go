4655365e01b501a48880fb896b55b1b187f8610c
Anastasios Karafyllidis <anastasios.karafyllidis@prodyna.com>
2023-03-10 17:47:09 +0200

    Added http post call to get msig alias info from api node

---
+++ service/node_service.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/chain4travel/camino-signavault/model"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type NodeServiceInterface interface {
+	GetMultisigAlias(alias string) string
+}
+
+type NodeService struct {
+}
+
+func NewNodeService() *NodeService {
+	return &NodeService{}
+}
+
+func (s *NodeService) GetMultisigAlias(alias string) (*model.AliasInfo, error) {
+	requestURL := "https://kopernikus.camino.network/ext/bc/P"
+	bodyReader := strings.NewReader(`
+			{
+				"jsonrpc":"2.0",
+				"id":1,
+				"method":"platform.getMultisigAlias",
+				"params":{
+					"Address":"` + alias + `"
+				}
+			}`)
+	req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		return nil, errors.New("error creating request: " + err.Error())
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, errors.New("client: error making http request: " + err.Error())
+	}
+	defer res.Body.Close()
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, errors.New("client: could not read response body: " + err.Error())
+	}
+
+	var aliasInfo *model.AliasInfo
+	err = json.Unmarshal(resBody, &aliasInfo)
+	if err != nil {
+		return nil, errors.New("could not unmarshal alias info: " + err.Error())
+	}
+
+	return aliasInfo, nil
+}
